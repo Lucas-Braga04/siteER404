643849a9e432401e6cdcb1be072bd9e97cad588e
Lucas-Braga04 <115594692+Lucas-Braga04@users.noreply.github.com>
2023-06-09 19:03:11 -0300

    .

--- js/scriptPitch.js
+++ js/scriptPitch.js
@@ -1,36 +1,3 @@
-<<<<<<< Updated upstream
-document.getElementById('downloadPitch').addEventListener('click', function(){
-    let video = document.getElementById('video');
-    let source = document.getElementsByTagName('source',[0]);
-    let videoURL = source.getAttribute('src');
-    let a = document.createElement('a')
-    a.href = videoURL;
-    a.download = 'https://www.youtube.com/watch?v=KVjBCT2Lc94&list=RDL2QTtdeL3dE&index=27';
-    a.click();
-})
-/*function downloadVideo() {
-    let videoUrl = 'https://www.youtube.com/watch?v=KVjBCT2Lc94&list=RDL2QTtdeL3dE&index=27';
-    
-    let video = document.createElement('video');
-    video.src = videoUrl;
-    video.controls = true;
-    
-    let anchor = document.createElement('a');
-    anchor.href = videoUrl;
-    anchor.target = '_blank';
-    anchor.download = '';
-    anchor.appendChild(video);
-    
-    let container = document.getElementById('videoContainer');
-    container.innerHTML = '';
-    container.appendChild(anchor);
-  }
-  */
-  /*function downloadVideo() {
-    let videoUrl = 'https://www.youtube.com/watch?v=KVjBCT2Lc94&list=RDL2QTtdeL3dE&index=27';
-    window.open(videoUrl);
-  }*/
-=======
 window.onload = function() {
     let downloadBtn = document.getElementById('downloadBtn');
     let video = document.getElementById('myVideo');
@@ -50,5 +17,4 @@ window.onload = function() {
       return parts[parts.length - 1];
     }
   };
->>>>>>> Stashed changes
   
